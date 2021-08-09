1dc5a2e550c931aaadc35edcb2028e068ebbb078
nugrohoff <fakhrl.fanani.nugroho@gmail.com>
2021-08-10 01:15:00 +0700

    use env to setting the app

--- application/config/database.php
+++ application/config/database.php
@@ -75,10 +75,10 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => 'root',
-	'password' => '',
-	'database' => 'db_inventori',
+	'hostname' => getenv("DB_HOST"),
+	'username' => getenv("DB_USER"),
+	'password' => getenv("DB_PASS"),
+	'database' => getenv("DB_NAME"),
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
---
+++ script.sh
@@ -0,0 +1,7 @@
+export BASE_URL=http://localhost:8080
+export DB_HOST=localhost
+export DB_NAME=inventori
+export DB_PASS=nugrohospace
+export DB_USER=nugrohospace
+
+php -S localhost:8080
